1d527f639179eee194eeeb8e8c8b4048e36b83e0
haemin4738 <leeheamin12@gmail.com>
2025-08-18 15:52:31 +0900

    testPostService -g

---
+++ src/main/java/com/ll/domain/testPost/service/TestPostService.java
@@ -0,0 +1,7 @@
+package com.ll.domain.testPost.service;
+
+import com.ll.framework.annotations.Service;
+
+@Service
+public class TestPostService {
+}
--- src/test/java/com/ll/framework/ioc/ApplicationContextTest.java
+++ src/test/java/com/ll/framework/ioc/ApplicationContextTest.java
@@ -1,15 +1,18 @@
 package com.ll.framework.ioc;
 
+import com.ll.domain.testPost.service.TestPostService;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ApplicationContextTest {
     private static ApplicationContext applicationContext;
 
     @BeforeAll
     public static void beforeAll() {
-        ApplicationContext applicationContext = new ApplicationContext("com.ll");
+        applicationContext = new ApplicationContext("com.ll");
         applicationContext.init();
     }
 
@@ -17,5 +20,14 @@ public class ApplicationContextTest {
     @DisplayName("ApplicationContext 객체 생성")
     public void t1() {
         System.out.println(applicationContext);
+    }
+
+    @Test
+    @DisplayName("testPostService 빈 얻기")
+    public void t2() {
+        TestPostService testPostService = applicationContext
+                .genBean("testPostService");
+
+        assertThat(testPostService).isNotNull();
     }
 }
