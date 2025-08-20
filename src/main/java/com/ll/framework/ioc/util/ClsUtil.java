aee83bcf05af7062e6454e047049a36aa6addacc
haemin4738 <leeheamin12@gmail.com>
2025-08-20 15:15:46 +0900

    ClsUtilTest -g

---
+++ src/main/java/com/ll/framework/ioc/util/ClsUtil.java
@@ -0,0 +1,11 @@
+package com.ll.framework.ioc.util;
+
+import lombok.SneakyThrows;
+
+public class ClsUtil {
+
+    @SneakyThrows
+    public static <T> Class<T> loadClass(String clsPath){
+        return (Class<T>)Class.forName(clsPath);
+    }
+}
